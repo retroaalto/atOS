3cf0ee7b21ddb468636d7cfc811ab45e0825962f
antako <114558368+Antonako1@users.noreply.github.com>
2024-07-22 00:09:29 +0300

    init

--- start.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-cd /d %~dp0 
-RMDIR /Q /S .\.git
-git init -b main
-git add .
-git commit -m "Initial commit"
-git status
-DEL start.sh
-DEL start.bat
--- z-linux/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Running project..."
-cd "$(dirname "$0")"
-. ./scripts/globals.sh
-cd ../build
-./linux/"$PROJECTNAME" "$@"
-cd ..
