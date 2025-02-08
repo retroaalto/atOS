43cf324cdc7b560393d38968dbc45f6b526d5145
antonako <114558368+Antonako1@users.noreply.github.com>
2025-02-09 01:51:08 +0200

    Updated assembler flags

--- BUILD/MAKE_BOOTLOADER.bat
+++ BUILD/MAKE_BOOTLOADER.bat
@@ -2,7 +2,7 @@
 :: Compile the bootloader
 :: Usage: MAKE_BOOTLOADER.bat
 :: Requires: NASM installed and in PATH
-:: Output: OUTPUT\BOOTLOADER\bootloader.bin
+:: Output: OUTPUT\BOOTLOADER\bootloader.BIN
 
 @echo off
 setlocal
@@ -25,7 +25,7 @@ if not exist "%OUTPUT_BOOTLOADER_DIR%" mkdir "%OUTPUT_BOOTLOADER_DIR%"
 ECHO Output Directory: %OUTPUT_BOOTLOADER_DIR%
 
 :: Compile bootloader
-call %ASSEMBLER% %ASSEMBLER_FLAGS% "%OUTPUT_BOOTLOADER_DIR%\BOOTLOADER.bin" "%SOURCE_BOOTLOADER_DIR%\BOOTLOADER.asm"
+call %ASSEMBLER% -f bin -o "%OUTPUT_BOOTLOADER_DIR%\BOOTLOADER.BIN" "%SOURCE_BOOTLOADER_DIR%\BOOTLOADER.asm"
 if %ERRORLEVEL% neq 0 (
     echo ERROR: Failed to compile bootloader.
     endlocal
