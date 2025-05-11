653f6577a13b9e45e99d4916dd6d2224bf83dac5
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-05-11 23:15:05 +0300

    Boots into 32-bit mode -a20, nmi and idt

--- SOURCE/KERNEL/KERNEL_ENTRY.asm
+++ SOURCE/KERNEL/KERNEL_ENTRY.asm
@@ -3,35 +3,81 @@
 ; Licensed under the MIT License. See LICENSE file in the project root for full license information.
 ;
 ; DESCRIPTION
-;     Kernel entry point. 
+;     16-bit kernel entry point. 
 ;     This file will switch to 32-bit protected mode and load the master kernel file.
-;     It will also do memory checks and other initializations.
-;     .\BIOS directory contains BIOS functions only to be used by this file.
-; 
+;
 ; AUTHORS
 ;     Antonako1
 ; 
 ; REVISION HISTORY
 ;     2025/02/09 - Antonako1
 ;         Initial version. Prints a welcome message.
+;     2025/5/11  - Antonako1
+;         Transforms into 32-bit mode, sets up memory, idt and gdtr
 ; 
 ; REMARKS
 ;     None
 
 
 [BITS 16]
-[ORG 0x1000]
+[ORG 0x1000:0x0000]
 
 start:
     mov [drive_number], dl
-    ; mov si, [boot_drive]
-    ; mov cx, 1
-    ; call PRINT_HEXN
 
     mov si, msg_greeting_1
-    call PRINT
-    jmp .hang
-.hang:
+    call PRINTLN
+
+    ; Set up the stack
+    xor ax, ax
+    mov ss, ax
+    mov sp, 0x7C00
+
+    ; Set up the data segment
+    mov ax, 0x0000
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    cli
+    ; Set up memory
+    ; INT 0x15, AX=0xE820
+    
+    ; Set up IDT to use BIOS calls
+
+
+
+    mov si, msg_kernel_end
+    call PRINTLN
+
+    ; Start the 32-bit protected mode
+    lgdt [GDTR]    ; load GDT register with start address of Global Descriptor Table
+    mov eax, cr0 
+    or al, 1       ; set PE (Protection Enable) bit in CR0 (Control Register 0)
+    mov cr0, eax
+
+    ; Perform far jump to selector 08h (offset into GDT, pointing at a 32bit PM code segment descriptor) 
+    ; to load CS with proper PM32 descriptor)
+    jmp 08h:PModeMain
+
+PModeMain:
+    ; load DS, ES, FS, GS, SS, ESP
+    mov ax, 0x10    ; data segment selector
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov eax, 0x7c00
+    mov esp, eax
+    
+KRNL_JMP:
+    ; Read KRNL.BIN from disk, load it into memory and jump to it
+    ; The kernel is loaded at 0x100000:0x0000
+
+    jmp hang
+hang:
     cli
     hlt
 
