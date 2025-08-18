ac270798c3c9f21ff7a11c171a86186258a1613c
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-08-18 07:07:14 +0300

    Inline assembly functions

---
+++ SOURCE/STD/ASM.h
@@ -0,0 +1,63 @@
+#ifndef STD_ASM_H
+#define STD_ASM_H
+
+#include "./ATOSMINDEF.h"
+
+STATIC INLINE void insw(U16 __port, void *__buffer, U32 __count) {
+    ASM VOLATILE("cld; rep insw"
+        : "+D"(__buffer), "+c"(__count)
+        : "d"(__port)
+        : "memory");
+}
+
+STATIC INLINE void outsw(U16 __port, const void *__buffer, U32 __count) {
+    ASM VOLATILE("cld; rep outsw"
+        : "+S"(__buffer), "+c"(__count)
+        : "d"(__port)
+        : "memory");
+}
+
+
+/*+++
+INx and OUTx functions
+---*/
+
+STATIC INLINE U8 inb(U16 __port) {
+    U8 __ret;
+    ASM VOLATILE("inb %1, %0"
+        : "=a"(__ret)
+        : "Nd"(__port));
+    return __ret;
+}
+STATIC INLINE U16 inw(U16 __port) {
+    U16 __ret;
+    ASM VOLATILE("inw %1, %0"
+        : "=a"(__ret)
+        : "Nd"(__port));
+    return __ret;
+}
+STATIC INLINE U32 inl(U16 __port) {
+    U32 __ret;
+    ASM VOLATILE("inl %1, %0"
+        : "=a"(__ret)
+        : "Nd"(__port));
+    return __ret;
+}
+
+STATIC INLINE VOID outb(U16 __port, U8 __value) {
+    ASM volatile ( "outb %b0, %w1" : : "a"(__value), "Nd"(__port) : "memory");
+}
+STATIC INLINE VOID outw(U16 __port, U16 __value) {
+    ASM volatile ( "outw %w0, %w1" : : "a"(__value), "Nd"(__port) : "memory");
+}
+STATIC INLINE VOID outl(U16 __port, U32 __value) {
+    ASM volatile ( "outl %0, %w1" : : "a"(__value), "Nd"(__port) : "memory");
+}
+
+
+
+
+STATIC INLINE VOID io_wait(VOID){
+    outb(0x80, 0);
+}
+#endif // STD_ASM_H
