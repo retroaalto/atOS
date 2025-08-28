7c68af928d7857cfda607c68d545b3ca7a73fad2
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-08-28 23:55:33 +0300

    Fixed hotreload

--- SCRIPTS/HOT_RELOAD.sh
+++ SCRIPTS/HOT_RELOAD.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Usage:
+# ./SCRIPTS/HOT_RELOAD.sh -> Opens qemu emulator with hot reload support
+# ./SCRIPTS/HOT_RELOAD.sh hot -> Triggers hot reload in running QEMU
 
 # Paths
 ISO_IMAGE="./OUTPUT/ISO/atOS.iso"
@@ -12,7 +15,7 @@ if [ -z "$1" ]; then
     make runlh
 else
     # Hot reload: change CDROM in running QEMU via monitor
-nc localhost 4444 <<EOF
+timeout 0.5 nc localhost 4444 <<EOF
 change ide1-cd0 ./OUTPUT/ISO/atOS.iso
 system_reset
 EOF
