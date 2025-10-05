737cbd167b32329b62fe0351f3ed2a7c970a2281
Antonako1 <114558368+Antonako1@users.noreply.github.com>
2025-10-05 17:49:10 +0300

    Extended TEST case

--- SOURCE/PROGRAMS/TEST1/TEST1.c
+++ SOURCE/PROGRAMS/TEST1/TEST1.c
@@ -1,6 +1,10 @@
 #include <STD/TYPEDEF.h>
 #include <STD/ASM.h>
 #include <DRIVERS/VIDEO/VBE.h>
+#include <PROGRAMS/TEST1/TEST1.h>
+
+// Declare all functions above main to avoid CPU faults
+U0 test_func_internal(U0);
 
 U32 _start(U0) {
     U32 x = 0;
@@ -15,8 +19,14 @@ U32 _start(U0) {
         }
         VBE_DRAW_LINE(x, y, x+100, y, VBE_RED);
         VBE_DRAW_STRING(10, 10, "red lines from atOShell", VBE_BLACK, VBE_GREEN);
+        test_func(); // from header
+        test_func_internal();
         VBE_UPDATE_VRAM();
-        // HLT;
     }
     return 1;
+}
+
+U0 test_func_internal(U0) {
+    VBE_DRAW_ELLIPSE(200, 200, 50, 30, VBE_BLUE);
+    return;
 }
